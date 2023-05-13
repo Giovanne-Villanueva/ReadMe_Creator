7cd8f27225235f1b3c5365083e1044aad9386ff6
Giovanne-Villanueva <gvv8106@gmail.com>
2023-05-12 18:57:16 -0700

    Inital prompts for read me and potential readme frame

--- index.js
+++ index.js
@@ -1,13 +1,82 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    'What is the title of your project? ', 
+    'Give me a description of your project: ', 
+    'Will your project include a table of Contents? ', 
+    'Enter in any installation rquirements for your project: ',
+    'Please enter in useage information for this project: ',
+    'Enter in any contributors, tutorials, or thrid party assets for the project: ',
+    'What license are you using for your project? ',
+    'Tests and questions for the project can be entered here: '];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(newREADME, data) {
+    newREADME=data;
+
+    return newREADME;
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+const licenseOp = ['MIT', 'Apache', 'GNU', 'BSD 2-Clause', 'BSD 3-Clause'];
+//const default = 'MIT';
+function init() {
+    inquirer
+    .prompt([
+        {
+            type:'input',
+            name:'title',
+            message: questions[0]
+        },
+        {
+            type:'input',
+            name:'description',
+            message: questions[1]
+        },
+        {
+            type:'confirm',
+            name:'tableContents',
+            message: questions[2]
+        },
+        {
+            type:'input',
+            name:'installation',
+            message: questions[3]
+        },
+        {
+            type:'input',
+            name:'usage',
+            message: questions[4]
+        },
+        {
+            type:'input',
+            name:'credits',
+            message: questions[5]
+        },
+        {
+            type:'checkbox',
+            name:'licenses',
+            message: questions[6],
+            choices:[...licenseOp]
+        },
+        {
+            type:'input',
+            name:'tests',
+            message: questions[7]
+        }
+    ])
+    .then(response => {
+        //
+        var newREADME = "";
+        //const newREADME = fs.readFile('newReadME.md', 'utf-8', error=> error ? console.log(error): created=ture);
+        //if(created) 
+        writeToFile(newREADME, response);
+        fs.writeFile('newReadME.md', newREADME, error=> error? console.log(error):console.log('We have Sucessfully made your README file'));
+    });
+}
 
 // Function call to initialize app
 init();
